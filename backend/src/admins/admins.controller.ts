d4c8049c31d5de844303e88a5fea0fba254b6585
Francis John Baldon <fjbaldon@gmail.com>
2025-07-17 16:11:24 +0800

    Fix admin cannot change credentials

--- backend/src/admins/admins.controller.ts
+++ backend/src/admins/admins.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Patch, Request } from '@nestjs/common';
+import { Body, Controller, Patch, Request, UseGuards } from '@nestjs/common';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { UpdateAdminDto } from './dto/update-admin.dto';
 import { Role } from 'src/shared/enums/role.enum';
 import { AdminsService } from './admins.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('admins')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class AdminsController {
     constructor(private readonly adminsService: AdminsService) { }
 
     @Patch('me')
     @Roles(Role.Admin)
     updateProfile(@Request() req, @Body() updateAdminDto: UpdateAdminDto) {
-        const adminId = req.user.sub; // Or req.user.userId
+        const adminId = req.user.userId;
         return this.adminsService.update(adminId, updateAdminDto);
     }
 }
