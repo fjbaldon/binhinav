a647eb0babe29bd5f035e0dda1950f0b6bdf0fd2
Francis John Baldon <fjbaldon@gmail.com>
2025-07-20 19:44:14 +0800

    Implement global exception filter

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'; // <-- Import
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,9 @@ async function bootstrap() {
       transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
     }),
   );
+
+  // Apply our custom global exception filter
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(3000);
 }
